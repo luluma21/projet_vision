42a395ad7a6cc4f4c0f959fe391d9cfefd79f77f
GaetanChataignon <gaetan.chataignon@telecom-st-etienne.fr>
2019-11-26 18:25:15 +0100

    test

--- display.cpp
+++ display.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 display::display()
 {
-    Mat image=imread("image_2366.jpeg");
+    Mat image=imread("D:\\Img_Projet_Vision\\IMG_2366.jpg");
     namedWindow("My Image");
     imshow("My Image",image);
     waitKey(0);
-
+// jade or labite
 }
